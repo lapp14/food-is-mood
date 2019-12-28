f0a9aeae0555ad2af611c2f9c5fab7180ab911a7
Dan Lapp <dan.lapp@points.com>
2019-12-27 21:10:13 -0500

    Added widgets for ingredients '

--- food-is-mood/recipes/views.py
+++ food-is-mood/recipes/views.py
@@ -15,15 +15,16 @@ log = logging.getLogger(__name__)
 engine = Engine()
 Session = sessionmaker(bind=engine.get())
 
+
 @contextmanager
 def session_scope():
     session = Session()
     try:
-        log.debug('session_scope(): trying session')
+        log.debug("session_scope(): trying session")
         yield session
         session.commit()
     except:
-        log.error('session_scope(): rolling back session')
+        log.error("session_scope(): rolling back session")
         session.rollback()
         raise
     finally:
@@ -33,77 +34,46 @@ def session_scope():
 def http_route_notfound(request):
     return HTTPNotFound()
 
-@view_config(route_name='add_user')
+
+@view_config(route_name="add_user")
 def add_user(request):
-    first_name = request.GET.getone('first_name')
-    last_name = request.GET.getone('last_name')
+    first_name = request.GET.getone("first_name")
+    last_name = request.GET.getone("last_name")
     user = User(first_name=first_name, last_name=last_name)
 
     with session_scope() as session:
         session.add(user)
-        new_user = session.query(User).filter_by(first_name=first_name, last_name=last_name).first()
+        new_user = (
+            session.query(User)
+            .filter_by(first_name=first_name, last_name=last_name)
+            .first()
+        )
 
         if new_user is user:
-            log.debug('add_user(): New user added, {new_user}'.format(new_user=new_user))
-            print('New user added, {new_user}'.format(new_user=new_user))
+            log.debug(
+                "add_user(): New user added, {new_user}".format(new_user=new_user)
+            )
+            print("New user added, {new_user}".format(new_user=new_user))
 
         all_users = session.query(User.first_name, User.last_name).all()
 
     return Response("<pre>" + "\n".join(map(str, all_users)) + "</pre>")
 
-@view_config(route_name='get_users', renderer='templates/get_users.jinja2')
-@view_config(route_name='get_users_json', renderer='json')
+
+@view_config(route_name="get_users", renderer="templates/get_users.jinja2")
+@view_config(route_name="get_users_json", renderer="json")
 def get_users(request):
     cookie = request.session
-    if 'counter' in cookie:
-        cookie['counter'] += 1
+    if "counter" in cookie:
+        cookie["counter"] += 1
     else:
-        cookie['counter'] = 1
+        cookie["counter"] = 1
 
     with session_scope() as session:
         all_users = session.query(User.first_name, User.last_name).all()
 
-    return {
-        'users': all_users,
-        'name': 'All Users',
-        'counter': cookie['counter'],
-    }
+    return {"users": all_users, "name": "All Users", "counter": cookie["counter"]}
 
-pages = {
-    '100': dict(
-        uid='100',
-        title='Page 100',
-        description='<em>100</em>',
-        steps=[
-            (1, 'Prep potatoes and vegetables'),
-            (2, 'Cook chicken'),
-            (3, 'Make potatoes'),
-            (4, 'Finish vegetables')
-        ]
-    ),
-    '101': dict(
-        uid='101',
-        title='Page 101',
-        description='<em>101</em>',
-        steps=[
-            (1, 'Prep potatoes and vegetables'),
-            (2, 'Cook chicken'),
-        ]
-    ),
-    '102': dict(
-        uid='102',
-        title='Page 102',
-        description='<em>102</em>',
-        steps=[
-            (1, 'Prep potatoes and vegetables'),
-            (2, 'Cook chicken'),
-            (3, 'Make potatoes'),
-            (4, 'Finish vegetables'),
-            (5, 'Step 5'),
-            (6, 'Step 6'),
-        ]
-    )
-}
 
 class RecipeViews(object):
     def __init__(self, request):
@@ -112,22 +82,22 @@ class RecipeViews(object):
     @property
     def recipe_form(self):
         schema = RecipePage()
-        return deform.Form(schema, buttons=('submit',))
+        return deform.Form(schema, buttons=("submit",))
 
     @property
     def reqts(self):
         return self.recipe_form.get_widget_resources()
 
-    @view_config(route_name='home_view', renderer='templates/home_view.jinja2')
+    @view_config(route_name="home_view", renderer="templates/home_view.jinja2")
     def home_view(self):
         recipes = DBSession.query(Recipe).order_by(Recipe.title)
-        return dict(title='Home View', pages=recipes)
+        return dict(title="Home View", pages=recipes)
 
-    @view_config(route_name='recipe_add', renderer='templates/recipe_add_edit.jinja2')
+    @view_config(route_name="recipe_add", renderer="templates/recipe_add_edit.jinja2")
     def recipe_add(self):
         form = self.recipe_form.render()
 
-        if 'submit' in self.request.params:
+        if "submit" in self.request.params:
             controls = self.request.POST.items()
             try:
                 appstruct = self.recipe_form.validate(controls)
@@ -135,33 +105,33 @@ class RecipeViews(object):
                 # Form is NOT valid
                 return dict(form=e.render())
 
-            new_title = appstruct['title']
-            new_body = appstruct['description']
+            new_title = appstruct["title"]
+            new_body = appstruct["description"]
             DBSession.add(Recipe(title=new_title, description=new_body))
 
             page = DBSession.query(Recipe).filter_by(title=new_title).one()
             new_uid = page.uid
 
             # Now visit new page
-            url = self.request.route_url('recipe_view', uid=new_uid)
+            url = self.request.route_url("recipe_view", uid=new_uid)
             return HTTPFound(url)
 
         return dict(form=form)
 
-    @view_config(route_name='recipe_view', renderer='templates/recipe_view.jinja2')
+    @view_config(route_name="recipe_view", renderer="templates/recipe_view.jinja2")
     def recipe_view(self):
-        uid = int(self.request.matchdict['uid'])
+        uid = int(self.request.matchdict["uid"])
         recipe = DBSession.query(Recipe).filter_by(uid=uid).one()
         return dict(page=recipe)
 
-    @view_config(route_name='recipe_edit', renderer='templates/recipe_add_edit.jinja2')
+    @view_config(route_name="recipe_edit", renderer="templates/recipe_add_edit.jinja2")
     def recipe_edit(self):
-        uid = int(self.request.matchdict['uid'])
+        uid = int(self.request.matchdict["uid"])
         recipe = DBSession.query(Recipe).filter_by(uid=uid).one()
 
         recipe_form = self.recipe_form
 
-        if 'submit' in self.request.params:
+        if "submit" in self.request.params:
             controls = self.request.POST.items()
             try:
                 appstruct = recipe_form.validate(controls)
@@ -169,43 +139,47 @@ class RecipeViews(object):
                 return dict(page=recipe, form=e.render())
 
             # Change the content and redirect to the view
-            recipe['title'] = appstruct['title']
-            recipe['description'] = appstruct['description']
+            recipe["title"] = appstruct["title"]
+            recipe["description"] = appstruct["description"]
 
             recipe.ingredients.clear()
-            for ingredient in appstruct['ingredients']:
-                recipe.ingredients.append(RecipeIngredient(
-                    ingredient=ingredient[1],
-                    shopping_list=ingredient[2],
-                ))
+            for ingredient in appstruct["ingredients"]:
+                recipe.ingredients.append(
+                    RecipeIngredient(
+                        ingredient=ingredient["ingredient"],
+                        shopping_list=ingredient["shopping_list"],
+                    )
+                )
 
             recipe.steps.clear()
-            for step in appstruct['steps']:
-                recipe.steps.append(RecipeStep(
-                    rank=step[0],
-                    step=step[1]
-                ))
+            for step in appstruct["steps"]:
+                recipe.steps.append(RecipeStep(rank=step[0], step=step[1]))
 
-            recipe['rank'] = int(appstruct['rank'])
-            url = self.request.route_url('recipe_view', uid=uid)
+            # recipe.tags.clear()
+            # for tag in appstruct['tags']:
+            #    recipe.tags.append(RecipeTag)
+
+            recipe["rank"] = int(appstruct["rank"])
+            url = self.request.route_url("recipe_view", uid=uid)
             return HTTPFound(url)
 
         appstruct = {
-            'title': recipe.title,
-            'description': recipe.description,
-            'steps': [(step.rank, step.step) for step in recipe.steps],
-            'ingredients': [(ingredient.recipe_id,
-                             ingredient.ingredient,
-                             ingredient.shopping_list)
-                            for ingredient in recipe.ingredients],
-            'tags': recipe.tags,
+            "title": recipe.title,
+            "description": recipe.description,
+            "steps": [(step.rank, step.step) for step in recipe.steps],
+            "ingredients": [
+                {
+                    "ingredient": ingredient.ingredient,
+                    "shopping_list": ingredient.shopping_list,
+                }
+                for ingredient in recipe.ingredients
+            ],
+            # 'tags': [tag.tag for tag in recipe.tags],
         }
 
-        if recipe.rank is not None:
-            appstruct['rank'] = recipe.rank
+        if recipe.rank:
+            appstruct["rank"] = recipe.rank
 
         form = recipe_form.render(appstruct)
 
-
         return dict(recipe=recipe, form=form)
-
