42429a49b3a78c3f4b04a253d18e7b10e15c6b72
Dan <d.lapp14@gmail.com>
2020-07-25 22:36:18 -0400

    Black format

--- food-is-mood/recipes/images.py
+++ food-is-mood/recipes/images.py
@@ -1,31 +1,32 @@
 import boto3, os, sys, uuid
 
-AWS_S3_BUCKET_NAME = os.environ['AWS_S3_BUCKET_NAME']
-BASE_PATH = os.environ['AWS_S3_BASE_PATH']
-VALID_FILE_EXTENSIONS = [ '.jpg', '.jpeg', '.png', '.gif' ]
-MIN_FILESIZE_BYTES = 15360   # 15 kb
-MAX_FILESIZE_BYTES = 15728640   # 15 Mb
-UPLOAD_ARGS = {
-    "ACL": 'public-read'
-}
+AWS_S3_BUCKET_NAME = os.environ["AWS_S3_BUCKET_NAME"]
+BASE_PATH = os.environ["AWS_S3_BASE_PATH"]
+VALID_FILE_EXTENSIONS = [".jpg", ".jpeg", ".png", ".gif"]
+MIN_FILESIZE_BYTES = 15360  # 15 kb
+MAX_FILESIZE_BYTES = 15728640  # 15 Mb
+UPLOAD_ARGS = {"ACL": "public-read"}
 
 ERROR_TEXT = dict(
     max_filesize="Files must be under {} Mb".format(MAX_FILESIZE_BYTES / 1024 / 1024),
     min_filesize="Files must be at least {} kb".format(MIN_FILESIZE_BYTES / 1024),
     missing_filename="Incorrect filename or filetype",
     invalid_extension="Valid filetypes are {}".format(VALID_FILE_EXTENSIONS),
-    attempt_delete_path='Attempting to delete path'
+    attempt_delete_path="Attempting to delete path",
 )
-    
 
 
 def _get_s3_resource():
     bucket_name = AWS_S3_BUCKET_NAME
-    session = boto3.Session(aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'], aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
-    return session.resource('s3')
+    session = boto3.Session(
+        aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+    )
+    return session.resource("s3")
+
 
 def _check_file_extension(image):
-    filename, file_extension = os.path.splitext(image['filename'])
+    filename, file_extension = os.path.splitext(image["filename"])
 
     if filename is None:
         raise Exception(ERROR_TEXT.missing_filename)
@@ -35,17 +36,19 @@ def _check_file_extension(image):
 
     return file_extension
 
+
 def _get_stream_filesize(filestream):
     filesize = filestream.seek(0, 2)
     filestream.seek(0, 0)
     return filesize
 
+
 def verify_image(image):
     # check filesize
-    filesize = _get_stream_filesize(image['fp'])
+    filesize = _get_stream_filesize(image["fp"])
     if filesize < MIN_FILESIZE_BYTES:
         raise Exception(ERROR_TEXT.min_filesize)
-    
+
     if filesize > MAX_FILESIZE_BYTES:
         raise Exception(ERROR_TEXT.max_filesize)
 
@@ -53,6 +56,7 @@ def verify_image(image):
 
     # check extension and return
     return _check_file_extension(image)
+
 
 def delete_image(url):
     if url is None:
@@ -64,10 +68,11 @@ def delete_image(url):
     s3 = _get_s3_resource()
     image = s3.Object(AWS_S3_BUCKET_NAME, url)
     image.delete()
-    print('>> Deleted image: {}'.format(url))
+    print(">> Deleted image: {}".format(url))
+
 
 def upload_image(image):
-    print('UPLOAD IMAGE')
+    print("UPLOAD IMAGE")
     file_extension = _check_file_extension(image)
     s3 = _get_s3_resource()
 
@@ -75,10 +80,12 @@ def upload_image(image):
     file = os.path.join(BASE_PATH, filename)
 
     config = boto3.s3.transfer.TransferConfig(use_threads=True, max_concurrency=1)
-    response = s3.meta.client.upload_fileobj(image['fp'], AWS_S3_BUCKET_NAME, file, ExtraArgs=UPLOAD_ARGS, Config=config)
+    response = s3.meta.client.upload_fileobj(
+        image["fp"], AWS_S3_BUCKET_NAME, file, ExtraArgs=UPLOAD_ARGS, Config=config
+    )
 
     object_acl = s3.ObjectAcl(AWS_S3_BUCKET_NAME, file)
-    object_acl.put(ACL='public-read')        
-    print('>> Uploaded image: {}'.format(file))
+    object_acl.put(ACL="public-read")
+    print(">> Uploaded image: {}".format(file))
 
     return file
--- food-is-mood/recipes/views.py
+++ food-is-mood/recipes/views.py
@@ -51,7 +51,7 @@ def add_user(request):
         if new_user is user:
             log.debug("add_user(): New user added, {new_user}".format(new_user=new_user))
             print("New user added, {new_user}".format(new_user=new_user))
-        
+
         all_users = session.query(User.first_name, User.last_name).all()
 
     return Response("<pre>" + "\n".join(map(str, all_users)) + "</pre>")
@@ -89,11 +89,11 @@ class RecipeViews(object):
 
     def add_recipe_image(self, recipe, image):
         if image is None:
-            print('>> No image to upload')
+            print(">> No image to upload")
             return
 
-        if image['filename'] == recipe.image_path:
-            print('>> Not changing existing image')
+        if image["filename"] == recipe.image_path:
+            print(">> Not changing existing image")
             return
 
         if recipe.image_path:
@@ -127,7 +127,7 @@ class RecipeViews(object):
 
     def get_recipe_image_url(self, recipe):
         if recipe.image_path:
-            return os.path.join(os.environ['AWS_S3_BASE_URL'], recipe.image_path)
+            return os.path.join(os.environ["AWS_S3_BASE_URL"], recipe.image_path)
 
     def get_recipe_tags(self, recipe):
         result = (
@@ -150,8 +150,10 @@ class RecipeViews(object):
         schema = RecipeImageUploadPage()
 
         if image_path:
-            return deform.Form(schema, buttons=("upload new image", "keep existing image", "delete image",))
-        
+            return deform.Form(
+                schema, buttons=("upload new image", "keep existing image", "delete image",)
+            )
+
         return deform.Form(schema, buttons=("upload image", "skip",))
 
     @property
@@ -251,8 +253,8 @@ class RecipeViews(object):
                 appstruct = recipe_image_form.validate(controls)
             except deform.ValidationFailure as e:
                 return dict(recipe=recipe, form=e.render())
-            
-            self.add_recipe_image(recipe, appstruct['image'])
+
+            self.add_recipe_image(recipe, appstruct["image"])
             url = self.request.route_url("recipe_view", uid=uid)
             return HTTPFound(url)
 
@@ -260,7 +262,7 @@ class RecipeViews(object):
             print("KEEP IMAGE")
             url = self.request.route_url("recipe_view", uid=uid)
             return HTTPFound(url)
-        
+
         if "delete_image" in self.request.params:
             print("DELETE IMAGE")
             if recipe.image_path:
@@ -269,18 +271,21 @@ class RecipeViews(object):
 
             url = self.request.route_url("recipe_view", uid=uid)
             return HTTPFound(url)
-        
+
         form = recipe_image_form.render()
-        
+
         image = self.get_recipe_image_url(recipe)
-        question = "Would you like to edit the image?" if recipe.image_path else "Would you like to add an image?"
+        question = (
+            "Would you like to edit the image?"
+            if recipe.image_path
+            else "Would you like to add an image?"
+        )
         template_data = dict(recipe=recipe, form=form, question=question)
-        
+
         if image:
-            template_data['image'] = image        
+            template_data["image"] = image
 
         return template_data
-
 
     @view_config(route_name="recipe_view", renderer="templates/recipe_view.jinja2")
     def recipe_view(self):
@@ -295,8 +300,8 @@ class RecipeViews(object):
         recipe_item = dict(recipe=recipe, tags=tags)
 
         if image:
-            print('>> Serving image: {}'.format(image))
-            recipe_item['image'] = image
+            print(">> Serving image: {}".format(image))
+            recipe_item["image"] = image
 
         return dict(item=recipe_item, links=links)
 
